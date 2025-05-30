382285fdae1bafa7d7e912d6f4951906879a2e73
usr <usr@PC1>
2025-05-30 09:34:14 +0300

    InitialCommit

---
+++ HackerRankChallenges/Helpers/InputHelper.fs
@@ -0,0 +1,34 @@
+﻿module InputHelper
+
+open System
+
+let inputToIntList() : int list =
+    let list = Console.ReadLine()
+    list.Split([|' '|], System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+    |> Array.toList
+
+let inputLinesToIntList() : int list =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.takeWhile (fun line -> not (String.IsNullOrWhiteSpace line))
+    |> Seq.map int
+    |> Seq.toList
+
+let inputToDouble() : double =
+    let rec readValue() =
+        match Double.TryParse(Console.ReadLine()) with
+        | true, value -> value
+        | false, _ -> 
+            printfn "Invalid input. Please try again."
+            readValue()
+    readValue()
+
+let inputToInt() : int=
+    let rec readValue() =
+        match Int32.TryParse(Console.ReadLine()) with
+        | true, value -> value
+        | false, _ ->  
+            printfn "Invalid input. Please try again."
+            readValue()
+    readValue()
+
---
+++ HackerRankChallenges/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open IntroductionModule;
+open InputHelper
+
+[<EntryPoint>]
+let main argv =
+
+    let factors = inputToIntList ()
+    let powers = inputToIntList ()
+    let range = inputToIntList ()
+
+    let left = float range.Head
+    let right = float (List.last range)
+
+    let (area, volume) = defineIntegral factors powers left right
+
+    area |> (printfn "%.4f")
+    volume |> (printfn "%.4f")
+
+    0
